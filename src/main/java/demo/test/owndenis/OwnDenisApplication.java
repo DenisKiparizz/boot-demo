2d02a60d58b02f26236cc886cbe645ab39b6f333
Denis <d.salodki@godeltech.com>
2020-06-02 08:52:44 +0300

    Initial commit

---
+++ src/main/java/demo/test/owndenis/OwnDenisApplication.java
@@ -0,0 +1,10 @@
+package demo.test.owndenis;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnDenisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OwnDenisApplication.class, args);
+	}
+}
